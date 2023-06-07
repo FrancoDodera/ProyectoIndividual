08ef0d41c79339073c91cd997328dc8a09a11fca
FrancoDodera <francododera@hotmail.com>
2023-06-07 16:55:08 -0300

    Backend
    Terminamos las rutas de activity

--- cr-pi-countries-main/server/src/controllers/activirtController.js
+++ cr-pi-countries-main/server/src/controllers/activirtController.js
@@ -1,9 +1,34 @@
-const postActivity = (req, res) => {
-  res.send("aca posteo las actividades");
+const { Activity } = require("../db");
+
+const postActivity = async (req, res) => {
+  const { name, difficulty, duration, seasons } = req.body;
+  console.log("Add activity", req.body);
+  const validateActivity = await Activity.findOne({
+    where: {
+      name: name,
+    },
+  });
+  if (!name || !difficulty || !duration || !seasons) {
+    res.status(404).json("Please Complete ALL");
+  }
+  if (validateActivity) {
+    res.status(404).json("This activity has already been added");
+  } else {
+    const { id } = req.body;
+    const newActivity = await Activity.create({
+      id,
+      name,
+      difficulty,
+      duration,
+      seasons,
+    });
+    res.status(200).json(newActivity);
+  }
 };
 
-const getActivity = (req, res) => {
-  res.send("aca tengo las actividaes");
+const getActivity = async (req, res) => {
+  const activity = await Activity.findAll();
+  res.status(200).send(activity);
 };
 
 module.exports = { getActivity, postActivity };
--- cr-pi-countries-main/server/src/models/Activity.js
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -6,18 +6,21 @@ module.exports = (sequelize) => {
   sequelize.define(
     "Activity",
     {
-      id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+      },
       name: { type: DataTypes.STRING, allowNull: false },
       difficulty: {
         type: DataTypes.ENUM(["1", "2", "3", "4", "5"]),
         allowNull: false,
       },
       duration: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        validate: {
-          min: 0,
-        },
+        type: DataTypes.STRING,
+        allowNull: false,
       },
       seasons: {
         type: DataTypes.ENUM(["Summer", "Spring", "Winter", "Autumn"]),
